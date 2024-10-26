32470ef2a1f79ec3143d1c3306328e5553d5b37c
박지영 <124862122+shd1495@users.noreply.github.com>
2024-10-26 17:17:52 +0900

    feat: "소켓 이벤트 구현"

---
+++ src/events/onConnection.js
@@ -0,0 +1,11 @@
+import { onData } from './onData.js';
+import { onEnd } from './onEnd.js';
+import { onError } from './onError.js';
+
+export const onConnection = (socket) => {
+  console.log(`Client connected from :${socket.remoteAddress}:${socket.remotePort}`);
+
+  socket.on('data', onData(socket));
+  socket.on('end', onEnd(socket));
+  socket.on('error', onError(socket));
+};
