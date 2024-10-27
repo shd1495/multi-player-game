bd58ce276490abd45ac005ebc29e2ceb874a53dd
박지영 <124862122+shd1495@users.noreply.github.com>
2024-10-28 01:03:45 +0900

    fix: "빈 버퍼 추가"

--- src/events/onConnection.js
+++ src/events/onConnection.js
@@ -5,6 +5,8 @@ import { onError } from './onError.js';
 export const onConnection = (socket) => {
   console.log(`Client connected from :${socket.remoteAddress}:${socket.remotePort}`);
 
+  socket.buffer = Buffer.alloc(0);
+
   socket.on('data', onData(socket));
   socket.on('end', onEnd(socket));
   socket.on('error', onError(socket));
