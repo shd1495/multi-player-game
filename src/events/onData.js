71f1277dc0669fe339f75f093dd078e225c8b037
박지영 <124862122+shd1495@users.noreply.github.com>
2024-10-28 01:05:58 +0900

    feat: "보통 패킷 파싱 구현"

--- src/events/onData.js
+++ src/events/onData.js
@@ -1,1 +1,36 @@
-export const onData = (socket) => {};
+import { config } from '../config/config.js';
+import { PACKET_TYPE } from '../constants/header.js';
+import CustomError from '../utils/error/customError.js';
+import { packetParser } from '../utils/parser/packetParser.js';
+
+export const onData = (socket) => async (data) => {
+  socket.buffer = Buffer.concat([socket.buffer, data]);
+
+  const totalHeaderLength = config.packet.totalLength + config.packet.typeLength; // 4 + 1
+
+  //buffer에 담긴 데이터의 길이가 헤더보다 길면 파싱 시작
+  while (socket.buffer.length > totalHeaderLength) {
+    const length = socket.buffer.readUInt32BE(0);
+    const packetType = socket.buffer.readUInt8BE(config.packet.totalLength);
+
+    if (socket.buffer.length >= length) {
+      const packet = socket.buffer.subarray(totalHeaderLength, length);
+      // 남은 부분
+      socket.buffer = socket.buffer.subarray(length);
+      try {
+        // 패킷 파서
+        switch (packetType) {
+          case PACKET_TYPE.PING:
+            break;
+          case PACKET_TYPE.NORMAL:
+            const result = packetParser(packet);
+            console.log(result);
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    } else {
+      break;
+    }
+  }
+};
