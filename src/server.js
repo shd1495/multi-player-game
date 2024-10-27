eb2bcf6eb1c8078d590a6b7964298bb3606736c6
박지영 <124862122+shd1495@users.noreply.github.com>
2024-10-28 01:09:33 +0900

    fix: "상수를 config의 환경 변수 사용으로 변경"

--- src/server.js
+++ src/server.js
@@ -1,15 +1,15 @@
 import net from 'net';
-import { PORT, HOST } from './constants/env.js';
 import { onConnection } from './events/onConnection.js';
 import initServer from './init/index.js';
+import { config } from './config/config.js';
 
 const server = net.createServer(onConnection);
 
 async function startServer() {
   try {
     await initServer();
-    server.listen(PORT, HOST, () => {
-      console.log(`서버가 ${HOST}:${PORT}에서 실행 중입니다.`);
+    server.listen(config.server.port, config.server.host, () => {
+      console.log(`서버가 ${config.server.host}:${config.server.port}에서 실행 중입니다.`);
       console.log(server.address());
     });
   } catch (error) {
