c9b39b7af5e3076689945a337048152a24e93c08
박지영 <124862122+shd1495@users.noreply.github.com>
2024-10-28 00:27:09 +0900

    feat: "서버 실행전 protobuf 파일 로드 추가"

--- src/server.js
+++ src/server.js
@@ -1,9 +1,21 @@
 import net from 'net';
 import { PORT, HOST } from './constants/env.js';
 import { onConnection } from './events/onConnection.js';
+import initServer from './init/index.js';
 
 const server = net.createServer(onConnection);
 
-server.listen(PORT, HOST, () => {
-  console.log(`서버가 ${HOST}:${PORT}에서 실행 중입니다.`);
-});
+async function startServer() {
+  try {
+    await initServer();
+    server.listen(PORT, HOST, () => {
+      console.log(`서버가 ${HOST}:${PORT}에서 실행 중입니다.`);
+      console.log(server.address());
+    });
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+}
+
+startServer();
