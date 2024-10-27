bc0b4d987a1150cb70b7c294798f499ea944ef1e
박지영 <124862122+shd1495@users.noreply.github.com>
2024-10-28 00:24:06 +0900

    feat: "패킷 추가"
    클라이언트 패킷과 같은 protobuf 패킷  추가

---
+++ src/protobuf/packetNames.js
@@ -0,0 +1,17 @@
+export const packages = {
+  common: {
+    Packet: 'common.Packet',
+  },
+  initial: {
+    InitialPacket: 'initial.InitialPacket',
+  },
+  game: {
+    UpdateLocationPayload: 'game.UpdateLocationPayload',
+  },
+  response: {
+    Response: 'response.Response',
+  },
+  gameNotification: {
+    UpdateLocation: 'gameNotification.UpdateLocation',
+  },
+};
