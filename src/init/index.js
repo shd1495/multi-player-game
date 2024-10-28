361f11ebf1f6432c67866fdd5d4fe15fd5eb1a7a
박지영 <124862122+shd1495@users.noreply.github.com>
2024-10-28 20:21:54 +0900

    fix: "테스트 커넥션 실행 추가"

--- src/init/index.js
+++ src/init/index.js
@@ -1,4 +1,6 @@
+import pools from '../db/database.js';
 import { addGameSession } from '../sessions/game.session.js';
+import testDbConnection from '../utils/testConnection.js';
 import { loadProtos } from './loadProtos.js';
 import { v4 as uuidv4 } from 'uuid';
 
@@ -7,6 +9,7 @@ const initServer = async () => {
     await loadProtos();
     const gameId = uuidv4();
     const gameSession = addGameSession(gameId);
+    await testDbConnection(pools.USER_DB, 'USER_DB');
     console.log(gameSession);
   } catch (error) {
     console.error(error);
