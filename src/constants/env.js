0d7b11c82457516a9f55188e0f996ef6e1199ebf
박지영 <124862122+shd1495@users.noreply.github.com>
2024-10-28 20:20:13 +0900

    fix: "db 정보 추가"

--- src/constants/env.js
+++ src/constants/env.js
@@ -7,3 +7,9 @@ export const HOST = process.env.HOST || 'localhost';
 export const PORT = process.env.PORT || '5555';
 
 export const VERSION = process.env.VERSION || '1.0.0';
+
+export const DB_NAME = process.env.DB_NAME || 'database';
+export const DB_USER = process.env.DB_USER || 'user';
+export const DB_PASSWORD = process.env.DB_PASSWORD || 'password';
+export const DB_HOST = process.env.DB_HOST || 'localhost';
+export const DB_PORT = process.env.DB_PORT || 3306;
