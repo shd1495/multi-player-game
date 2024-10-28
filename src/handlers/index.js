deac875bae81e8beec12391014baf3eb19763372
박지영 <124862122+shd1495@users.noreply.github.com>
2024-10-28 17:44:29 +0900

    feat: "핸들러 맵핑, 핸들러 배정 기능 추가"

--- src/handlers/index.js
+++ src/handlers/index.js
@@ -1,9 +1,23 @@
 import { HANDLER_IDS } from '../constants/handlerIds.js';
+import CustomError from '../utils/error/customError.js';
+import { ErrorCodes } from '../utils/error/errorCodes.js';
+import initialHandler from './user/initial.handler.js';
 
 const handlers = {
   [HANDLER_IDS.INIT]: {
+    handler: initialHandler,
     protoType: 'initial.InitialPayload',
   },
+};
+
+export const getHandlerById = (handlerId) => {
+  if (!handlers[handlerId]) {
+    throw new CustomError(
+      ErrorCodes.UNKNOWN_HANDLER_ID,
+      `핸들러를 찾을 수 없습니다: ID ${handlerId}`,
+    );
+  }
+  return handlers[handlerId].handler;
 };
 
 export const getProtoTypeNameByHandlerId = (handlerId) => {
