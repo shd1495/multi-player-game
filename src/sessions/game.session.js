9f683ab30dc9ddac3de6d482346fe52db9839ca4
박지영 <124862122+shd1495@users.noreply.github.com>
2024-10-28 17:42:07 +0900

    fix: "gameSessoins 오타 수정"

--- src/sessions/game.session.js
+++ src/sessions/game.session.js
@@ -1,17 +1,17 @@
 import Game from '../classes/models/game.class.js';
-import { gameSession } from './sessions.js';
+import { gameSessions } from './sessions.js';
 
 export const addGameSession = (id) => {
   const session = new Game(id);
 
-  gameSession.push(session);
+  gameSessions.push(session);
   return session;
 };
 
 export const removeGameSession = () => {
-  gameSession.splice(0, 1);
+  gameSessions.splice(0, 1);
 };
 
 export const getGameSession = () => {
-  return gameSession[0];
+  return gameSessions[0];
 };
