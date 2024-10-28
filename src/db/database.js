b06f2c2175f073ae3cdf45bbbfe5b9dbc3d70c18
박지영 <124862122+shd1495@users.noreply.github.com>
2024-10-28 20:18:53 +0900

    feat: "db 커넥션풀 생성"

---
+++ src/db/database.js
@@ -0,0 +1,39 @@
+import mysql from 'mysql2/promise';
+import { config } from '../config/config.js';
+import { formatDate } from '../utils/dateFormatter.js';
+
+const { databases } = config;
+
+const createPool = (dbConfig) => {
+  const pool = mysql.createPool({
+    host: dbConfig.host,
+    port: dbConfig.port,
+    user: dbConfig.user,
+    password: dbConfig.password,
+    database: dbConfig.name,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0,
+  });
+
+  const originalQuery = pool.query;
+
+  pool.query = (sql, params) => {
+    const date = new Date();
+    // 쿼리 실행시 로그
+    console.log(
+      `[${formatDate(date)}] Executing query: ${sql} ${
+        params ? `, ${JSON.stringify(params)}` : ``
+      }`,
+    );
+    return originalQuery.call(pool, sql, params);
+  };
+
+  return pool;
+};
+
+const pools = {
+  USER_DB: createPool(databases.USER_DB),
+};
+
+export default pools;
