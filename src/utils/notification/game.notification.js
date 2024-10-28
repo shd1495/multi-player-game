8022648592f6d7419ed5e2687828c8c39c8228a0
박지영 <124862122+shd1495@users.noreply.github.com>
2024-10-28 19:24:30 +0900

    feat: "위치 패킷 구현 및 직렬화 추가"

---
+++ src/utils/notification/game.notification.js
@@ -0,0 +1,26 @@
+import { config } from '../../config/config.js';
+import { PACKET_TYPE } from '../../constants/header.js';
+import { getProtoMessages } from '../../init/loadProtos.js';
+
+const serializer = (message, type) => {
+  const packetLength = Buffer.alloc(config.packet.totalLength);
+  packetLength.writeUInt32BE(
+    message.length + config.packet.totalLength + config.packet.typeLength,
+    0,
+  );
+
+  const packetType = Buffer.alloc(config.packet.typeLength);
+  packetType.writeUInt8(type, 0);
+
+  return Buffer.concat([packetLength, packetType, message]);
+};
+
+export const createLocationPacket = (users) => {
+  const protoMessages = getProtoMessages();
+  const Location = protoMessages.gameNotification.UpdateLocation;
+
+  const payload = { users };
+  const message = Location.create(payload);
+  const locationPacket = Location.encode(message).finish();
+  return serializer(locationPacket, PACKET_TYPE.LOCATION);
+};
