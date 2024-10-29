b056fb329e5064e036c17cccd64dd0cdd710144c
박지영 <124862122+shd1495@users.noreply.github.com>
2024-10-29 23:35:41 +0900

    fix: "유저를 찾을 때 사용할 값 deviceId로 변경"

--- src/db/user/user.queries.js
+++ src/db/user/user.queries.js
@@ -1,6 +1,6 @@
 export const SQL_QUERIES = {
   FIND_USER_BY_DEVICE_ID: 'SELECT * FROM user WHERE device_id = ?',
   CREATE_USER: 'INSERT INTO user (id, device_id) VALUES (?, ?)',
-  UPDATE_USER_LOGIN: 'UPDATE user SET last_login = CURRENT_TIMESTAMP WHERE id = ?',
-  UPDATE_USER_POSITION: 'UPDATE user SET x = ?, y = ? WHERE id = ?',
+  UPDATE_USER_LOGIN: 'UPDATE user SET last_login = CURRENT_TIMESTAMP WHERE device_id = ?',
+  UPDATE_USER_POSITION: 'UPDATE user SET x = ?, y = ? WHERE device_id = ?',
 };
