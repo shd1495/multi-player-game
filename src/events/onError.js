1af87a14e4265a0fe28ca376567cf7aaf5cad825
박지영 <124862122+shd1495@users.noreply.github.com>
2024-10-29 23:36:11 +0900

    feat: "onError 이벤트 구현"

--- src/events/onError.js
+++ src/events/onError.js
@@ -1,1 +1,15 @@
-export const onError = (socket) => (error) => {};
+import { handleError } from '../utils/error/errorHandler.js';
+import CustomError from '../utils/error/customError.js';
+import { removeUser } from '../sessions/user.session.js';
+import { getGameSession } from '../sessions/game.session.js';
+
+export const onError = (socket) => async (err) => {
+  console.error('소켓 오류:', err);
+  handleError(socket, new CustomError(500, `소켓 오류: ${err.message}`));
+
+  // 세션에서 유저 삭제
+  await removeUser(socket);
+
+  const gameSession = getGameSession();
+  gameSession.removeUser(socket);
+};
