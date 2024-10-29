4d12a383193953711782b9b75d74d2629334d820
박지영 <124862122+shd1495@users.noreply.github.com>
2024-10-29 23:36:01 +0900

    feat: "onEnd 이벤트 구현"

--- src/events/onEnd.js
+++ src/events/onEnd.js
@@ -1,1 +1,12 @@
-export const onEnd = (socket) => (data) => {};
+import { removeUser } from '../sessions/user.session.js';
+import { getGameSession } from '../sessions/game.session.js';
+
+export const onEnd = (socket) => async () => {
+  console.log('클라이언트 연결이 종료되었습니다.');
+
+  // 세션에서 유저 삭제
+  await removeUser(socket);
+
+  const gameSession = getGameSession();
+  gameSession.removeUser(socket);
+};
