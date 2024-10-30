e41258808d20153dae8a312488046516f46455d0
박지영 <124862122+shd1495@users.noreply.github.com>
2024-10-31 00:19:21 +0900

    fix: "latency에 따른 위치 계산 추가 및 핑 송수신 추가"

--- src/classes/models/user.class.js
+++ src/classes/models/user.class.js
@@ -1,3 +1,5 @@
+import { createPingPacket } from '../../utils/notification/game.notification.js';
+
 class User {
   constructor(socket, id, playerId, latency, position) {
     this.id = id;
@@ -9,6 +11,11 @@ class User {
     this.lastX = 0;
     this.lastY = 0;
     this.lastUpdateTime = Date.now();
+    this.speed = 3;
+  }
+
+  isSocketConnected() {
+    return this.socket && !this.socket.destroyed;
   }
 
   updatePosition(x, y) {
@@ -20,23 +27,43 @@ class User {
   }
 
   calculatePosition(latency) {
-    if (this.x === this.lastX && this.y === this.lastY) {
-      return {
-        x: this.x,
-        y: this.y,
-      };
-    }
+    const hasMoved = this.x !== this.lastX || this.y !== this.lastY;
+    if (!hasMoved) return { x: this.x, y: this.y };
 
+    // 이동 시간이 짧거나 latency가 작으면 timeDiff도 적어지고, distance도 작아짐
     const timeDiff = (Date.now() - this.lastUpdateTime + latency) / 1000; // 초 단위
-    const speed = 1; // 초당 1씩
-    const distance = speed * timeDiff;
-    const directionX = this.x !== this.lastX ? Math.sign(this.x - this.lastX) : 0;
-    const directionY = this.y !== this.lastY ? Math.sign(this.y - this.lastY) : 0;
+    const distance = this.speed * timeDiff;
+
+    // 방향 계산
+    const directionX = Math.sign(this.x - this.lastX);
+    const directionY = Math.sign(this.y - this.lastY);
 
     return {
       x: this.x + directionX * distance,
       y: this.y + directionY * distance,
     };
+  }
+
+  ping() {
+    console.log(this.isSocketConnected());
+    if (this.isSocketConnected()) {
+      const now = Date.now();
+      console.log(`${this.id} ping`);
+      this.socket.write(createPingPacket(now), (err) => {
+        if (err) {
+          console.error(`Error sending ping to ${this.id}:`, err);
+          this.cleanup(); // 사용자 청소
+        }
+      });
+    } else {
+      console.log(`${this.id} cannot ping, socket is destroyed`);
+    }
+  }
+
+  handlePong(data) {
+    const now = Date.now();
+    this.latency = (now - data.timestamp) / 2;
+    console.log(`pong ${this.id} at ${now} latency ${this.latency}`);
   }
 }
 
--- src/events/onData.js
+++ src/events/onData.js
@@ -1,6 +1,10 @@
 import { config } from '../config/config.js';
 import { PACKET_TYPE } from '../constants/header.js';
 import { getHandlerById } from '../handlers/index.js';
+import { getProtoMessages } from '../init/loadProtos.js';
+import { getUserBySocket } from '../sessions/user.session.js';
+import CustomError from '../utils/error/customError.js';
+import { ErrorCodes } from '../utils/error/errorCodes.js';
 import { packetParser } from '../utils/parser/packetParser.js';
 
 export const onData = (socket) => async (data) => {
@@ -21,6 +25,14 @@ export const onData = (socket) => async (data) => {
         // 패킷 파서
         switch (packetType) {
           case PACKET_TYPE.PING:
+            const protoMessages = getProtoMessages();
+            const ping = protoMessages.common.Ping;
+            const pingMessage = ping.decode(packet);
+            const user = getUserBySocket(socket);
+            if (!user) {
+              throw new CustomError(ErrorCodes.USER_NOT_FOUND, '유저를 찾을 수 없습니다.');
+            }
+            user.handlePong(pingMessage);
             break;
           case PACKET_TYPE.NORMAL:
             const { handlerId, userId, payload } = packetParser(packet);
