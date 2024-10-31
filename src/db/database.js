14e209409ed26d81802d42e1bc6af151a770cbf1
박지영 <124862122+shd1495@users.noreply.github.com>
2024-10-31 22:53:42 +0900

    fix: "안쓰는 모듈 삭제"

--- src/db/database.js
+++ src/db/database.js
@@ -1,6 +1,5 @@
 import mysql from 'mysql2/promise';
 import { config } from '../config/config.js';
-import { formatDate } from '../utils/formatters/dateFormatter.js';
 
 const { databases } = config;
 
